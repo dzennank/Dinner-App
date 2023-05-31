bab5405148afbc5462401c7ab8667ad8efbd7913
dzennank <dzenankrlic@gmail.com>
2023-05-31 14:35:57 +0200

    Jwt token generator

--- BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using BuberDuinner.Application.Common.Interfaces.Authentication;
 using Microsoft.IdentityModel.Tokens;
-using System.Security.Cryptography;
 
 namespace BuberDinner.Infrastructure.Authentication
 {
@@ -15,20 +15,17 @@ namespace BuberDinner.Infrastructure.Authentication
     {
         public string TokenGenerator(Guid userId, string firstName, string lastName)
         {
-
-            // Generate a 256-bit key
-               var key = new byte[32]; // 256 bits = 32 bytes
-                using (var rng = RandomNumberGenerator.Create())
-                {
-                    rng.GetBytes(key);
-                }
-
-                // Convert the key to a Base64-encoded string
-                var base64Key = Convert.ToBase64String(key);
-
+                // Generate a random key
+            //     var key = new byte[32]; // 256 bits = 32 bytes
+            //     using (var rng = RandomNumberGenerator.Create())
+            //     {
+            //         rng.GetBytes(key);
+            //     }
+            
+            // var base64Key = Convert.ToBase64String(key);
             var signingCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(
-                    Encoding.UTF8.GetBytes(base64Key)),
+                    Encoding.UTF8.GetBytes("super-secret-key-super-secret-key")),
                     SecurityAlgorithms.HmacSha256
                 );
             
